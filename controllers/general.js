36d9c2f4002b19234156b4112ee59406cfe2ef6f
nagasudhirpulla <nagasudhirpulla@gmail.com>
2017-07-15 18:23:54 +0530

    undefined problem sorted

--- controllers/general.js
+++ controllers/general.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
 router.get('/home', function (req, res, next) {
     //console.log((typeof req.user == 'undefined') ? "undefined" : req.user.username);
     var epc_person = null;
-    if (req.user != undefined && req.user.role_str == 0 && req.user.epc_users_id != undefined && req.user.epc_users_id != null && !isNaN(req.user.epc_users_id)) {
+    if (typeof req.user != "undefined" && req.user.role_str == 0 && typeof req.user.epc_users_id != "undefined" && req.user.epc_users_id != null && !isNaN(req.user.epc_users_id)) {
         // the users is an epc client
         EpcUser.getDetailedById(req.user.epc_users_id, function (err, rows) {
             if (err) {
